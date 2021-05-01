23f81e8ea21a95f0df1b85ed16edd25c185031c5
chaima-naweni <80164560+chaima-naweni@users.noreply.github.com>
2021-05-01 06:18:06 +0100

    Add files via upload

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(adminsTableSeeder::class);
+        $this->call(clientsTableSeeder::class);
+    
     }
 }

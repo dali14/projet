345b7ab67068096bf0d0141d97c1317d4c7f94eb
sfaxiikram <sfaxiikram2@gmail.com>
2021-05-01 02:02:21 +0100

    second-commit

---
+++ app/Detailproduit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detailproduit extends Model
+{
+   
+    public function produit()
+    {
+        return $this->belongsTo('App\Produit');
+    }
+}

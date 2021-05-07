a8447d1e1a80b53026bfcebed1a2fbf4951485ea
chaima-naweni <chaima.naoueni14@gmail.com>
2021-05-07 02:39:07 +0100

    template-admin

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/','HomeController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function (){
+
+    return view('admin.dashbord');
+});

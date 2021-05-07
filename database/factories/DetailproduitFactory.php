a014daedfa986fe08c25d9ce28ccc63de16d3457
sfaxiikram <80152541+sfaxiikram@users.noreply.github.com>
2021-05-07 23:23:51 +0100

    Add files via upload

--- database/factories/DetailproduitFactory.php
+++ database/factories/DetailproduitFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Detailproduit;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Detailproduit::class, function (Faker $faker) {
     return [
-        //
+        'dateentree'=>&faker->datetime,
+        'datesortie'=>&faker->datetime,
+        'quantite'=>&<faker->randomNumber,
+        'Produit_id'=>Produit::get('id')->random()
     ];
 });
+/*      $table->increments('id');
+            $table->date('dateentree');
+            $table->date('datesortie');
+            $table->integer('quantite');
+            $table->integer('Produit_id')->unsigned();
+            $table->foreign('Produit_id')->references('id')->on('produits')->onDelete('restrict')->onUpdate('restrict');
+            $table->timestamps();*/ 
--- database/factories/ProduitFactory.php
+++ database/factories/ProduitFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Produit;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Produit::class, function (Faker $faker) {
     return [
-        //
+
+        'numproduit'=>&faker->randomNumber,
+        'nomproduit'=>$faker->word,
+        'typeproduit'=>&faker->sentence,
+        'created_at'=>now()
+
+      
     ];
 });
+/*$table->integer('numproduit');
+            $table->string('nomproduit');
+            $table->string('typeproduit');
+            $table->double('prixdevente', 8, 3); */
